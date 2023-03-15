faadfba031ee2b22a485590a6d92b3d396301ccc
David Magalhães <81379039+davmag91@users.noreply.github.com>
2023-03-15 16:22:35 +0000

    add fonts and organize grid

---
+++ lib/screens/categories_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import '../components/category_item.dart';
+import '../data/dummy_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Vamos Cozinhar'),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(25),
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+          mainAxisExtent: 120,
+        ),
+        children: DUMMY_CATEGORIES.map((category) {
+          return CategoryItem(
+            category: category,
+          );
+        }).toList(),
+      ),
+    );
+  }
+}
