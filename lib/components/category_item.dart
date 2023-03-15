2b6ca6d5473ed1f2b59370954428eb391ac3afae
David Magalhães <81379039+davmag91@users.noreply.github.com>
2023-03-15 16:36:12 +0000

    Add themes

--- lib/components/category_item.dart
+++ lib/components/category_item.dart
@@ -10,17 +10,21 @@ class CategoryItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.all(15),
-      child: Text(category!.title),
       decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(15),
-          gradient: LinearGradient(
-            colors: [
-              category!.color.withOpacity(0.5),
-              category!.color,
-            ],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-          )),
+        borderRadius: BorderRadius.circular(15),
+        gradient: LinearGradient(
+          colors: [
+            category!.color.withOpacity(0.5),
+            category!.color,
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: Text(
+        category!.title,
+        style: Theme.of(context).textTheme.titleMedium,
+      ),
     );
   }
 }
