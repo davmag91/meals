61d23cf1d3740e24b93603d7badad05694875e0e
David Magalhães <81379039+davmag91@users.noreply.github.com>
2023-03-16 18:54:52 +0000

    route data, new screen, and screen details

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:meals/screens/categories_meals_screen.dart';
 import 'package:meals/screens/categories_screen.dart';
+import 'package:meals/screens/meal_detail_screen.dart';
 
 import 'utils/app_routes.dart';
 
@@ -32,6 +33,15 @@ class MyApp extends StatelessWidget {
       routes: {
         AppRoutes.HOME: (context) => const CategoriesScreen(),
         AppRoutes.CATEGORIES_MEALS: (context) => const CategoriesMealsScreen(),
+        AppRoutes.MEAL_DETAIL: (context) => const MealDetailScreen()
+      },
+      // onGenerateRoute: (settings) {
+
+      // },
+      onUnknownRoute: (settings) {
+        return MaterialPageRoute(
+          builder: (context) => const CategoriesScreen(),
+        );
       },
     );
   }
--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:meals/components/meal_item.dart';
+import 'package:meals/data/dummy_data.dart';
 import '../models/category.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
@@ -8,12 +10,21 @@ class CategoriesMealsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final category = ModalRoute.of(context)!.settings.arguments as Category;
 
+    final categoriesMeals = dummyMeals.where((meal) {
+      return meal.categories.contains(category.id);
+    }).toList();
+
     return Scaffold(
       appBar: AppBar(
         title: Text(category!.title),
       ),
       body: Center(
-        child: Text('Recipes by Category id ${category!.id}'),
+        child: ListView.builder(
+          itemBuilder: (ctx, index) {
+            return MealItem(meal: categoriesMeals[index]);
+          },
+          itemCount: categoriesMeals.length,
+        ),
       ),
     );
   }
